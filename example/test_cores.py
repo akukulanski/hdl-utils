4726a0bbb720f18235f850c7b9d6a3652f595b45
akukulanski <akukulanski@gmail.com>
2024-08-20 16:27:59 -0300

    test in test_uart moved to test_cores

--- example/test_cores.py
+++ example/test_cores.py
@@ -69,3 +69,25 @@ class TestbenchCoresVerilog(TemplateTestbenchVerilog):
         self.run_testbench(
             verilog_sources, top_level, test_module,
             parameters=parameters, vcd_file=vcd_file)
+
+    @pytest.mark.parametrize('uart_clk_div', [
+                                15,
+                                # 16
+                            ])
+    def test_uart_rx(self, uart_clk_div):
+        verilog_sources = [
+            'example/uart/uart_rx.v',
+        ]
+        # top level HDL
+        top_level = 'uart_rx'
+        # name of cocotb test module
+        test_module = 'uart.tb_uart_rx'
+        # Waveform file
+        vcd_file = './uart_rx.v.vcd'
+        # Parameters
+        parameters = {
+            'UART_CLK_DIV': uart_clk_div,
+        }
+        self.run_testbench(
+            verilog_sources, top_level, test_module,
+            parameters=parameters, vcd_file=vcd_file)
